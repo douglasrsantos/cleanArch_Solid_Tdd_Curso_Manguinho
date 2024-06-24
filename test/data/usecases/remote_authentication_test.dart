20d4b27dd1d49e55f2c8076452726d000064511b
Douglas Santos <douglasrosasantos@gmail.com>
2024-06-24 08:44:53 -0300

    move production code to their own target

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,40 +1,16 @@
 import 'package:faker/faker.dart';
-import 'package:fordev/domain/usecases/usecases.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'remote_authentication_test.mocks.dart';
 
+import 'package:fordev/domain/usecases/usecases.dart';
+
+import 'package:fordev/data/usecases/usecases.dart';
+import 'package:fordev/data/http/http.dart';
+
 @GenerateNiceMocks([MockSpec<HttpClient>()])
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({
-    required this.httpClient,
-    required this.url,
-  });
-
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
-
-    await httpClient.request(
-      url: url,
-      method: 'post',
-      body: body,
-    );
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    required String url,
-    required String method,
-    Map body,
-  });
-}
-
 void main() {
   late MockHttpClient httpClient;
   late RemoteAuthentication sut;
