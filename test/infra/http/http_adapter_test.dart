430ee5fa79d01663697215e57d9b24dbe7dfd1e0
Douglas Santos <douglasrosasantos@gmail.com>
2024-06-25 19:52:17 -0300

    ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/annotations.dart';
@@ -15,6 +17,7 @@ class HttpAdapter {
   Future<void> request({
     required String url,
     required String method,
+    Map? body,
   }) async {
     final headers = {
       'content-type': 'application/json',
@@ -24,6 +27,7 @@ class HttpAdapter {
     await client.post(
       Uri.parse(url),
       headers: headers,
+      body: jsonEncode(body),
     );
   }
 }
@@ -38,10 +42,14 @@ void main() {
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
   });
-  
+
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: {'any_key': 'any_value'},
+      );
 
       verify(client.post(
         Uri.parse(url),
@@ -49,6 +57,7 @@ void main() {
           'content-type': 'application/json',
           'accept': 'application/json',
         },
+        body: '{"any_key":"any_value"}',
       ));
     });
   });
