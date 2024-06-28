10734a031e4ec95de0bef569ac0cc32ca1ad9bee
Douglas Santos <douglasrosasantos@gmail.com>
2024-06-28 19:13:08 -0300

    move HttpAdapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,48 +1,14 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/data/http/http.dart';
-
 import 'http_adapter_test.mocks.dart';
 
+import 'package:fordev/infra/http/http.dart';
+
 @GenerateNiceMocks([MockSpec<Client>()])
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  @override
-  Future<Map?> request({
-    required String url,
-    required String method,
-    Map? body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-
-    final jsonBody = body != null ? jsonEncode(body) : null;
-
-    final response = await client.post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonBody,
-    );
-
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else {
-      return null;
-    }
-  }
-}
-
 void main() {
   late HttpAdapter sut;
   late MockClient client;
