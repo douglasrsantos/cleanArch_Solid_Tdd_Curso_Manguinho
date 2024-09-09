490a3535270d187cb3caaf963fa90a2018c7211f
Douglas Santos <douglasrosasantos@gmail.com>
2024-09-09 08:32:00 -0300

    ensure LoginPage enables button if form is valid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -65,9 +65,14 @@ class LoginPage extends StatelessWidget {
                           );
                         }),
                   ),
-                  const ElevatedButton(
-                    onPressed: null,
-                    child: Text('ENTRAR'),
+                  StreamBuilder<bool>(
+                    stream: presenter?.isFormValidStream,
+                    builder: (context, snapshot) {
+                      return ElevatedButton(
+                        onPressed: snapshot.data == true ? (){} : null,
+                        child: const Text('ENTRAR'),
+                      );
+                    }
                   ),
                   TextButton.icon(
                     onPressed: () {},
